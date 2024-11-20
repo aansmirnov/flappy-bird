ab1cf8e819e89761b268d7bc9e8a1f6c9b2a11b1
aansmirnov <aansmrnv@gmail.com>
2024-11-21 00:52:02 +0400

    fix alias

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     alias: [
       {
         find: "@",
-        replacement: resolve(__dirname, "@"),
+        replacement: resolve(__dirname, "src"),
       },
     ],
   },
